8291c63cc6a947f1f983f90b03e6877b10ede641
stephane-nguyen <stepdh01@gmail.com>
2023-12-21 13:55:17 +0100

    Add mnist with perceptron multi-layer and gradiant retro prop

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -1,6 +1,6 @@
-import os
 import struct
 import random
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -21,95 +21,144 @@ def read_images(filename):
         images = images.reshape(num_items, rows, cols)
     return images
 
-def display_random_pattern(images, labels):
+def get_random_pattern(images, labels):
     index = random.randint(0, len(images) - 1)
-    plt.imshow(images[index], cmap='gray')
-    plt.title(f'Label: {labels[index]}')
-    plt.show()
-    print(f'Ydk Class: {labels[index]}')
     return labels[index], index
 
-def set_weights(input_size):
-    weights = initialize_weights(input_size)
-    weights = normalize_weights(weights)
-    return weights
+def set_weights(num_inputs, num_neurons):
+    weights = initialize_weights(num_inputs, num_neurons)
+    normalized_weights = normalize_weights(weights)
+    return normalized_weights
 
-def initialize_weights(num_weights):
-    return [random.uniform(-1, 1) for _ in range(num_weights)]
+def initialize_weights(num_inputs, num_neurons):
+    return [[random.uniform(-1, 1) for _ in range(num_inputs)] for _ in range(num_neurons)]
 
-def normalize_weights(W):
-    total = sum(W)
-    return [w / total for w in W]
+def normalize_weights(weights):
+    normalized_weights = []
+    for neuron_weights in weights:
+        weight_sum = sum(neuron_weights)
+        # Avoid division by zero
+        if weight_sum == 0:
+            normalized_neuron_weights = neuron_weights
+        else:
+            normalized_neuron_weights = [weight / weight_sum for weight in neuron_weights]
+        normalized_weights.append(normalized_neuron_weights)
+    return normalized_weights
 
-def propagate_on_retina(X):
-    return X / 255
+def propagate_on_retina(image):
+    return image.flatten() / 255.0
 
-def calculate_output_neuron(potential):
-    return sigmoid(potential)
+# Calculate X_h
+def calculate_hidden_layer_output(input_vector, weights_input_hidden):
+    hidden_layer_output = []
+    hidden_neuron = range(len(weights_input_hidden))
+    input_neuron = range(len(input_vector))
+    
+    for j in hidden_neuron: 
+        pot_h = 0
+        for i in input_neuron:  
+            pot_h += input_vector[i] * weights_input_hidden[j][i]
+        hidden_layer_output.append(sigmoid(pot_h))
+    return hidden_layer_output, pot_h
 
-
-def calculate_sum(X, W):
-    return sum(x * w for x, w in zip(X, W))
+# Calculate X_i
+def calculate_output_layer_output(hidden_layer_output, weights_hidden_output):
+    output_layer_output = []
+    output_neuron = range(len(weights_hidden_output))
+    hidden_neuron = range(len(hidden_layer_output))
+    
+    for k in output_neuron:  
+        pot_i = 0
+        for j in hidden_neuron:  
+            pot_i += hidden_layer_output[j] * weights_hidden_output[k][j]
+        output_layer_output.append(sigmoid(pot_i))
+    return output_layer_output, pot_i
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    """Compute the sigmoid function safely for large `x`."""
+    if x < -700:  # Values less than -700 will cause overflow in `exp`.
+        return 0.0
+    elif x > 700:  # Values greater than 700 will lead to a sigmoid very close to 1.
+        return 1.0
+    return 1 / (1 + math.exp(-x))
 
-def calculate_delta_output_layer(pot_i, labels, x_i):
+def calculate_delta_output_layer(pot_i, label, x_i):
     delta = []
     for i in range(len(x_i)):
-        delta.append(sigmoid_derivative(pot_i) * labels[i] - x_i[i])
+        # Calculate the error term for the output neuron
+        # Correct class is 1, rest are 0s
+        expected_value = 1.0 if i == label else 0.0
+        delta.append(sigmoid_derivative(pot_i) * (expected_value - x_i[i]))
     return delta
 
-def calculate_delta_hidden_layer(delta_i, pot_h, weights, x_h):
-    delta = []
-    for _ in range(len(x_h)):
-        delta.append(sigmoid_derivative(pot_h) * calculate_sum(delta_i, weights))
-    return delta
+def calculate_delta_hidden_layer(pot_h, delta_output, weights_hidden_output, x_h):
+    delta_hidden = []
+    for j in range(len(x_h)):
+        # Calculate the error term for the hidden neuron
+        error_term = sum(delta_output[k] * weights_hidden_output[k][j] for k in range(len(delta_output)))
+        delta_hidden.append(sigmoid_derivative(pot_h) * error_term)
+    return delta_hidden
 
 def sigmoid_derivative(x):
     return sigmoid(x) * (1 - sigmoid(x))
 
 def learn(weights, delta_i, epsilon, x_j):
     for i in range(len(weights)):
-        weights[i] += epsilon * delta_i * x_j
+        weights[i] += epsilon * delta_i * x_j[i]
     return weights
 
-def calculate_error_percentage(delta_i, p):
-    print(delta_i)
- 
-    all_deltas = np.concatenate(delta_i)
-    # Calculate the sum of the absolute values of the errors
-    error_sum = np.sum(abs(all_deltas))
-    # Return the mean error percentage
-    print(error_sum)
-    return error_sum / (p * len(delta_i))
+def calculate_error_percentage(delta_output, p):
+    error_sum = np.sum(np.abs(delta_output))
+    error_percentage = (error_sum / len(delta_output)) * 100
+    return error_percentage / p
 
-def main():
+def launch_network(label, image):
     input_size = 28 * 28
     hidden_size = 100
     output_size = 10
     threshold = 0.04
+    epsilon = 0.01 # Learning rate
+    
+    error_percentages = [] # plotting purpose
     error_percentage = float("inf")
     while error_percentage > threshold:
       
-        weights = set_weights(input_size)
-        weights_input_hidden = set_weights(hidden_size)
+        weights_input_hidden = set_weights(input_size, hidden_size)
+        weights_hidden_output  = set_weights(hidden_size, output_size)
 
         labels = read_labels('train-labels.idx1-ubyte')
         images = read_images('train-images.idx3-ubyte')
-        pattern, p = display_random_pattern(images, labels)
+        label, image_index = get_random_pattern(images, labels)
 
-        x_j = [propagate_on_retina(image) for image in images]
+        x_j = propagate_on_retina(images[image_index])
+        x_h, pot_h = calculate_hidden_layer_output(x_j, weights_input_hidden)
+        x_i, pot_i = calculate_output_layer_output(x_h, weights_hidden_output)
 
-        pot_h = calculate_sum(x_j, weights)
-        x_h = calculate_output_neuron(pot_h)
+        delta_i = calculate_delta_output_layer(pot_i, label, x_i)
+        delta_h = calculate_delta_hidden_layer(pot_h, delta_i, weights_hidden_output, x_h)
 
-        pot_i = calculate_sum(x_h, weights_input_hidden)
-        x_i = calculate_output_neuron(pot_i)
+        # Update the weights for the hidden-output layer
+        for k in range(output_size):
+            weights_hidden_output[k] = learn(weights_hidden_output[k], delta_i[k], epsilon, x_h)
 
-        delta_i = calculate_delta_output_layer(pot_i, labels, x_i)
-        delta_h = calculate_delta_hidden_layer(delta_i, x_i, weights_input_hidden, x_h)
-        error_percentage = calculate_error_percentage(delta_i, p)
+        # Update the weights for the input-hidden layer
+        for j in range(hidden_size):
+            weights_input_hidden[j] = learn(weights_input_hidden[j], delta_h[j], epsilon, x_j)
 
+        error_percentage = calculate_error_percentage(delta_i, 100) 
+        error_percentages.append(error_percentage)  
+        print(f'Current error percentage: {error_percentage:.2f}%')
+    plt.plot(error_percentages)
+    plt.xlabel('Iteration') 
+    plt.ylabel('Error Percentage')
+    plt.title('Error Percentage Over Iterations') 
+    plt.show()
+    
+def main():
+    # Train base
+    launch_network('train-labels.idx1-ubyte', 'train-images.idx3-ubyte')
+    # Test base
+    launch_network('t10k-images-idx3-ubyte', 't10k-labels-idx1-ubyte')
+    
 if __name__ == "__main__":
     main()
